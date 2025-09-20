19389b6aae28ca4d0f4045a3d23a0bb609bd8d32
mdipasquale315 <31933669+mdipasquale315@users.noreply.github.com>
2025-09-20 11:33:52 -0400

    Update Signin.js

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
+    fetch('https://smart-brain-backend-l6cv.onrender.com', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
