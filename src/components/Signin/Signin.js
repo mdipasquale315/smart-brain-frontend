b7c7a0108836ab553192abb8dc0bd10674876195
mdipasquale315 <31933669+mdipasquale315@users.noreply.github.com>
2025-09-21 19:02:07 -0400

    Update Signin.js

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -6,22 +6,21 @@ class Signin extends React.Component {
     this.state = {
       signInEmail: '',
       signInPassword: ''
-    };
+    }
   }
 
   onEmailChange = (event) => {
-    this.setState({ signInEmail: event.target.value });
-  };
+    this.setState({signInEmail: event.target.value})
+  }
 
   onPasswordChange = (event) => {
-    this.setState({ signInPassword: event.target.value });
-  };
+    this.setState({signInPassword: event.target.value})
+  }
 
-  onSubmitSignIn = (event) => {
-    event.preventDefault(); // Prevent default form submission
+  onSubmitSignIn = () => {
     fetch('https://smart-brain-backend-l6cv.onrender.com/signin', {
       method: 'post',
-      headers: { 'Content-Type': 'application/json' },
+      headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
         email: this.state.signInEmail,
         password: this.state.signInPassword
@@ -30,14 +29,11 @@ class Signin extends React.Component {
       .then(response => response.json())
       .then(user => {
         if (user.id) {
-          this.props.loadUser(user);
+          this.props.loadUser(user)
           this.props.onRouteChange('home');
         }
       })
-      .catch(err => {
-        console.log('Error signing in:', err);
-      });
-  };
+  }
 
   render() {
     const { onRouteChange } = this.props;
@@ -45,59 +41,39 @@ class Signin extends React.Component {
       <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
         <main className="pa4 black-80">
           <div className="measure">
-            <fieldset id="sign_in" className="ba b--transparent ph0 mh0">
+            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
               <legend className="f1 fw6 ph0 mh0">Sign In</legend>
-              {/* Wrap inputs inside a form */}
-              <form onSubmit={this.onSubmitSignIn}>
-                <div className="mt3">
-                  <label
-                    className="db fw6 lh-copy f6"
-                    htmlFor="email-address"
-                  >
-                    Email
-                  </label>
-                  <input
-                    className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                    type="email"
-                    name="email-address"
-                    id="email-address"
-                    onChange={this.onEmailChange}
-                    required
-                    autoComplete="email"
-                  />
-                </div>
-                <div className="mv3">
-                  <label
-                    className="db fw6 lh-copy f6"
-                    htmlFor="password"
-                  >
-                    Password
-                  </label>
-                  <input
-                    className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                    type="password"
-                    name="password"
-                    id="password"
-                    onChange={this.onPasswordChange}
-                    required
-                    autoComplete="current-password"
-                  />
-                </div>
-                {/* Submit button inside form */}
+              <div className="mt3">
+                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                 <input
-                  className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-                  type="submit"
-                  value="Sign in"
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="email"
+                  name="email-address"
+                  id="email-address"
+                  onChange={this.onEmailChange}
                 />
-              </form>
+              </div>
+              <div className="mv3">
+                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                <input
+                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="password"
+                  name="password"
+                  id="password"
+                  onChange={this.onPasswordChange}
+                />
+              </div>
             </fieldset>
+            <div className="">
+              <input
+                onClick={this.onSubmitSignIn}
+                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                type="submit"
+                value="Sign in"
+              />
+            </div>
             <div className="lh-copy mt3">
-              <p
-                onClick={() => onRouteChange('register')}
-                className="f6 link dim black db pointer"
-              >
-                Register
-              </p>
+              <p  onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
             </div>
           </div>
         </main>
