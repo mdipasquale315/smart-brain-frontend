15956213907dbfab55d8f9a12d063abdaa7d7718
mdipasquale315 <31933669+mdipasquale315@users.noreply.github.com>
2025-09-21 16:46:28 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -63,36 +63,23 @@ class App extends Component {
     this.setState({input: event.target.value});
   }
 
-  onButtonSubmit = () => {
-    this.setState({imageUrl: this.state.input});
-      fetch('https://smart-brain-backend-l6cv.onrender.com/imageurl', {
-        method: 'post',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({
-          input: this.state.input
-        })
-      })
-      .then(response => response.json())
-      .then(response => {
-        if (response) {
-          fetch('https://smart-brain-backend-l6cv.onrender.com/image', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-              id: this.state.user.id
-            })
-          })
-            .then(response => response.json())
-            .then(count => {
-              this.setState(Object.assign(this.state.user, { entries: count}))
-            })
-            .catch(console.log)
-
-        }
-        this.displayFaceBox(this.calculateFaceLocation(response))
-      })
-      .catch(err => console.log(err));
-  }
+ // In your component
+onButtonSubmit = () => {
+  fetch('https://smart-brain-backend-l6cv.onrender.com/imageurl', {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ input: this.state.input }),
+  })
+  .then(res => res.json())
+  .then(data => {
+    if (data.faceBoxes) {
+      this.displayFaceBoxes(this.calculateFaceLocations(data.faceBoxes));
+    } else {
+      this.setState({ boxes: [] }); // handle no faces detected
+    }
+  })
+  .catch(err => console.log(err));
+};
 
   onRouteChange = (route) => {
     if (route === 'signout') {
