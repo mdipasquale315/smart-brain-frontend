2cc4252bf4eab3182bcc18f42bf8f916588817c3
mdipasquale315 <31933669+mdipasquale315@users.noreply.github.com>
2025-09-21 18:45:28 -0400

    Update Signin.js

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -19,7 +19,7 @@ class Signin extends React.Component {
 
   onSubmitSignIn = (event) => {
     event.preventDefault(); // Prevent default form submission
-    fetch('http://localhost:3000/signin', {
+    fetch('https://smart-brain-backend-l6cv.onrender.com/signin', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
