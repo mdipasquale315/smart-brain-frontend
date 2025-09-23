cd3704085949bf097412573cb286a76dd8ac6b18
mdipasquale315 <31933669+mdipasquale315@users.noreply.github.com>
2025-09-23 19:58:15 -0400

    Update FaceRecognition.js

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,15 +1,32 @@
+// src/components/FaceRecognition/FaceRecognition.js
 import React from 'react';
-import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl, box }) => {
-  return (
-    <div className='center ma'>
-      <div className='absolute mt2'>
-        <img id='inputimage' alt='' src={imageUrl} width='500px' heigh='auto'/>
-        <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
-      </div>
+const FaceRecognition = ({ box, imageUrl }) => (
+  <div className="center ma">
+    <div className="absolute mt2" style={{ position: 'relative' }}>
+      <img
+        id="inputimage"
+        src={imageUrl}
+        alt=""
+        width="500px"
+        height="auto"
+      />
+      {/* Map over all boxes and create overlay divs */}
+      {box.map((b, i) => (
+        <div
+          key={i}
+          style={{
+            position: 'absolute',
+            boxShadow: '0 0 0 3px #149df2 inset',
+            top: b.topRow,
+            left: b.leftCol,
+            width: b.rightCol - b.leftCol,
+            height: b.bottomRow - b.topRow,
+          }}
+        />
+      ))}
     </div>
-  );
-}
+  </div>
+);
 
 export default FaceRecognition;
